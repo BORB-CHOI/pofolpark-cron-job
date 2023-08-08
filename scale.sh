6711a7151d82184946b687ac1704ab3ff5ab8128
BORB <qzsec299@naver.com>
2023-08-08 22:19:17 +0900

    Update scale.sh

--- scale.sh
+++ scale.sh
@@ -3,7 +3,7 @@
 curl --request POST \
      --url https://api.render.com/v1/services/$SERVICE_ID/jobs \
      --header 'accept: application/json' \
-     --header 'authorization: Bearer $TOKEN' \
+     --header 'authorization: Bearer' $TOKEN \
      --header 'content-type: application/json' \
      --data '
 {
