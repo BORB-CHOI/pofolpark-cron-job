88bb60f5ed65f5ba0d5615820d4ea2591dd6d20a
BORB <qzsec299@naver.com>
2023-08-08 21:50:01 +0900

    Update scale.sh

--- scale.sh
+++ scale.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
 
-curl --request POST \
-     --url https://api.render.com/v1/services/$SERVICE_ID/jobs \
-     --header 'accept: application/json' \
-     --header 'authorization: Bearer $TOKEN' \
-     --header 'content-type: application/json' \
-     --data '
-{
-  "startCommand": "python manage.py update_weather"
-}
-'
+echo $SERVICE_ID
+echo $TOKEN
